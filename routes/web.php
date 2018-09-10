c07e0f6564a7562539410c826a68f4a59bf0cfdd
lordofxtreme <kingfahad0802@gmail.com>
2018-09-10 20:15:02 +0600

    v0

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/', 'PagesController@getHome');
+Route::get('/about', 'PagesController@getAbout');
+Route::get('/contact', 'PagesController@getContact');
 Route::post('/contact/submit', 'MessagesController@submit');
 Route::get('/messages', 'MessagesController@getMessages');
