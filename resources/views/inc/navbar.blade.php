4c27f327baca46544cf807200d6db2bb55a3f137
lordofxtreme <kingfahad0802@gmail.com>
2018-09-10 20:35:36 +0600

    'v0'

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,23 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-        <a class="navbar-brand" href="/about">Anisul Islam</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExample09">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item ">
-              <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/about">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="/contact">Contact</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-md-0">
-            <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-          </form>
-        </div>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h3 class="my-0 mr-md-auto font-weight-normal">Anisul Islam</h3>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark {{Request::is('/') ? 'active' : ''}}" href="/">Home</a>
+        <a class="p-2 text-dark {{Request:: is('about') ? 'active' : ''}}" href="/about">About</a>
       </nav>
+      <a class="btn btn-outline-primary" href="/contact">Contact</a>
+    </div>
